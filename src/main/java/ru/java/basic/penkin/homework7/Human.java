a75073c42ef7b60f0d475029d51c261840c0ebbe
penkinsergey <penkinsergeykms@gmail.com>
2023-11-23 01:51:42 +0800

    Initial commit

---
+++ src/main/java/ru/java/basic/penkin/homework7/Human.java
@@ -0,0 +1,140 @@
+package ru.java.basic.penkin.homework7;
+
+class Human {          // В гитхаб мастер ветка
+    private String name;
+    private Transport currentTransport;
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCurrentTransport(Transport transport) {
+        this.currentTransport = transport;
+    }
+
+    public boolean move(int distance, TerrainType terrainType) {
+        if (currentTransport != null) {
+            return currentTransport.move(distance, terrainType);
+        } else {
+            System.out.println(name + " идет пешком " + distance + " километров по " + terrainType);
+            return true;
+        }
+    }
+
+    public void getOffTransport() {
+        if (currentTransport != null) {
+            currentTransport = null;
+            System.out.println(name + " сошел с транспорта");
+        } else {
+            System.out.println(name + " не сидит ни в транспорте, ни на лошади");
+        }
+    }
+}
+
+enum TerrainType {      // Перечисление (enum) разных типов местности. На лекции было сказано, что перечисление должны быть с заглавных букв и, видимо, перечисление надо делать на английском.
+    DENSEFOREST,
+    PLAIN,
+    SWAMP
+}
+
+interface Transport {               // Интерфейс для транспорта
+    boolean move(int distance, TerrainType terrainType);
+}
+
+class Car implements Transport {   // Класс Машина
+
+    private int fuel;
+
+    public Car(int fuel) {
+        this.fuel = fuel;
+    }
+
+    @Override
+    public boolean move(int distance, TerrainType terrainType) {
+        if (terrainType == TerrainType.DENSEFOREST || terrainType == TerrainType.SWAMP) {
+            System.out.println("Машина не может ехать по " + terrainType);
+            return false;
+        }
+
+        int fuelConsumption = distance / 10;
+        if (fuel >= fuelConsumption) {
+            fuel -= fuelConsumption;
+            System.out.println("Машина проехала " + distance + " километров по " + terrainType);
+            return true;
+        } else {
+            System.out.println("У машины закончилось топливо");
+            return false;
+        }
+    }
+}
+
+class Horse implements Transport {       // Класс Лошадь
+    private int energy;
+
+    public Horse(int energy) {
+        this.energy = energy;
+    }
+
+    @Override
+    public boolean move(int distance, TerrainType terrainType) {
+        if (terrainType == TerrainType.SWAMP) {
+            System.out.println("Лошадь с ездоком не может передвигаться по " + terrainType);
+            return false;
+        }
+
+        int energyConsumption = distance / 5;
+        if (energy >= energyConsumption) {
+            energy -= energyConsumption;
+            System.out.println("Лошадь с ездоком проскакала " + distance + " километров по " + terrainType);
+            return true;
+        } else {
+            System.out.println("У лошади недостаточно энергии, чтобы двигаться дальше");
+            return false;
+        }
+    }
+}
+
+class OffRoadCar implements Transport {   // Класс Вездеход
+    private int fuel;
+
+    public OffRoadCar(int fuel) {
+        this.fuel = fuel;
+    }
+
+    @Override
+    public boolean move(int distance, TerrainType terrainType) {
+        System.out.println("Вездеход проехал " + distance + " километров по " + terrainType);
+        return true;
+    }
+}
+
+class Bicycle implements Transport {   // Класс Велосипед
+
+    private int humanEnergy;
+
+    public Bicycle(int humanEnergy) {
+        this.humanEnergy = humanEnergy;
+    }
+
+    @Override
+    public boolean move(int distance, TerrainType terrainType) {
+        if (terrainType == TerrainType.SWAMP) {
+            System.out.println("Велосипед не может двигаться по " + terrainType);
+            return false;
+        }
+
+        int energyConsumption = distance / 2;
+        if (humanEnergy >= energyConsumption) {
+            humanEnergy -= energyConsumption;
+            System.out.println("Человек на велосипеде проехал " + distance + " километров по " + terrainType);
+            return true;
+        } else {
+            System.out.println("У человека нет сил, чтобы ехать дальше на велосипеде");
+            return false;
+        }
+    }
+}
---
+++ src/main/java/ru/java/basic/penkin/homework7/Main.java
@@ -0,0 +1,32 @@
+package ru.java.basic.penkin.homework7;
+
+public class Main {           // В гитхаб мастер ветка
+    public static void main(String[] args) {
+        Human human = new Human("Владимир");
+
+        Car car = new Car(20);
+        human.setCurrentTransport(car);
+        human.move(120, TerrainType.PLAIN);
+        human.getOffTransport();
+
+        human.move(5, TerrainType.PLAIN);
+
+        Horse horse = new Horse(100);
+        human.setCurrentTransport(horse);
+        human.move(15, TerrainType.DENSEFOREST);
+        human.move(10, TerrainType.SWAMP);
+        human.getOffTransport();
+
+
+        OffRoadCar offRoadcar = new OffRoadCar(10);
+        human.setCurrentTransport(offRoadcar);
+        human.move(40, TerrainType.SWAMP);
+        human.getOffTransport();
+
+        Bicycle bicycle = new Bicycle(60);
+        human.setCurrentTransport(bicycle);
+        human.move(50, TerrainType.PLAIN);
+        human.move(100, TerrainType.PLAIN);
+
+    }
+}
